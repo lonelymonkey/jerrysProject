81c2b637e602fdf82e8a3c13fb0304a71b189bbe
wayne <waynechen0709@gmail.com>
2016-06-04 00:49:02 -0700

    added example php files

---
+++ tourismsurfing/index.php
@@ -0,0 +1,21 @@
+<?php
+
+include '../includes/global.inc';
+
+
+
+printHeader('hello world');
+
+$var1 = 'hello world';
+
+echo '<link href="css/style_common.css" rel="stylesheet">';
+
+printFooter();
+?>
+
+<h1>
+klsjfg;sdjls;djf'l
+<?php
+ echo $var1;
+?>
+</h1>
