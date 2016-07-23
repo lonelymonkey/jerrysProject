18d4c547cb9f4d649bb4826b55b780949aaa14b8
Jerry <zoj3zoj3@gmail.com>
2016-07-23 15:51:35 -0700

    power of 2 is done, backspace is done, answer is done

--- instalation.php
+++ instalation.php
@@ -2,15 +2,11 @@
 include 'includes/JerryDatabass.class.inc';
 include 'includes/config.inc';
 $database = new Database();
-
-
-
-
-$database->query("CREATE DATABASE instal_practice");
+$database->query("CREATE DATABASE Jerry_install_practice");
 $database->execute();
-$database->query("use instal_practice");
+$database->query("use Jerry_install_practice");
 $database->execute();
-$database->query("CREATE TABLE `lets check` (
+$database->query("CREATE TABLE `Jerry's table` (
   `id` int(11) NOT NULL auto_increment,
   `name` varchar(64) NOT NULL,
   `email`varchar(64) NOT NULL,
