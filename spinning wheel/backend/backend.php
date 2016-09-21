f742ce58e3b36d1629883cd1a02629582c71b97d
jerry hung <jerry@GG3V0.(none)>
2016-09-20 17:05:32 -0700

    able to insert all the data into corrisbonding table

--- spinning wheel/backend/backend.php
+++ spinning wheel/backend/backend.php
@@ -9,8 +9,6 @@ $whole_set = $_POST;
 
 
 
-
-
 # when user click spin, the $bar is not empty, so php will give a randon number to front end, and front end also
 # send the whole set to us as assocaitive array
 if ($whole_set) {
@@ -20,8 +18,8 @@ if ($whole_set) {
   $distribution = $whole_set["sample"];
   $color = $whole_set["color"];
   // insert set name into wheelset
-  $randon_number = rand(0,100);
-  echo $randon_number*0.01;
+  $randon_number = rand(0,100)*0.01;
+  echo $randon_number;
   $database->query('INSERT INTO Wheelset (name, createdDate)
   VALUES (:name,NOW())');
   $database->bind(':name',$setname);
@@ -34,10 +32,12 @@ if ($whole_set) {
   $desire_setID = $set_id_from_DB[count($set_id_from_DB)-1];
   //var_dump($desire_setID["setId"]);
   for ($i=0; $i < count($whole_set["id"]); $i++) {
-    inert_into_probabilityslice($desire_setID["setId"], $slice_name[$i],$distribution[$i],$color[$i]);
+    insert_into_probabilityslice($desire_setID["setId"], $slice_name[$i],$distribution[$i],$color[$i]);
   }
 
-
+  $winner_entry = decide_output($randon_number,$distribution);
+  //var_dump($slice_name);
+  insert_into_wheelsetresult($desire_setID["setId"], $winner_entry, $distribution[$winner_entry], $color[$winner_entry]);
 
   //$database->query('SELECT * FROM Wheelset where ');
   //$database->execute();
@@ -57,9 +57,38 @@ else {
   $dataset = ["Jerry"=>"best","bill"=>"hehe"];
   var_dump($dataset);
 }
+function insert_into_wheelsetresult ($setId, $winner_entry, $sample, $color){
+  global $database;
+  $database->query('INSERT INTO wheelsetresult (setId, winner, distribution, colorCode)
+  VALUES (:setId,:winner,:distribution,:colorCode)');
+  $database->bind(':setId',$setId)->bind(':winner',$winner_entry)->bind(':distribution',$sample)->bind(':colorCode',$color);
+  $database->execute();
+}
+function decide_output($winner_number,$distribution){
+ $total = 0;
+ $total_prob = 0;
+ $target = 0;
+ $probability_array = array();
+ for ($i=0; $i < count($distribution) ; $i++) {
+   $total += $distribution[$i];
+ }
+ for ($i=0; $i < count($distribution); $i++) {
+   array_push($probability_array, $distribution[$i]/$total);
+ }
+ for ($i=0; $i < count($distribution); $i++) {
+   $total_prob = $total_prob + $probability_array[$i];
+ //  console.log("total_prob is " + total_prob);
 
-function inert_into_probabilityslice($setId, $name, $sample, $color) {
-  $database = new Database();
+   if ($winner_number < $total_prob) {
+     $target = $i;
+     break;
+   }
+
+ }
+ return $target;
+}
+function insert_into_probabilityslice($setId, $name, $sample, $color) {
+  global $database;
   $database->query('INSERT INTO probabilitySlice (setId, name, distribution, colorCode)
   VALUES (:setId,:name,:distribution,:colorCode)');
   $database->bind(':setId',$setId)->bind(':name',$name)->bind(':distribution',$sample)->bind(':colorCode',$color);
