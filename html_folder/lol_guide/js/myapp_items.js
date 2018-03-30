6b35980e74e25a6d739c6ea795faa4376fadf0fb
Jerry <zoj3zoj3@gmail.com>
2018-03-30 17:08:57 +0800

    Finish the item filter with catagory and changing the style of the scroll bar

--- html_folder/lol_guide/js/myapp_items.js
+++ html_folder/lol_guide/js/myapp_items.js
@@ -15,7 +15,8 @@
                                   items : ""
                               };
   var second_item_from = "";
-
+  var local_item_set = [];
+  var local_item_tags = [];
   myApp.item_search = function (){
     var item_keyword = "";
     var item_str = "";
@@ -98,7 +99,7 @@
 
                       </div>
                       <div class = "item_pool_catagory">
-                          item_pool_catagory
+
                       </div>
                     </div>
                 </div>
@@ -106,8 +107,192 @@
      </div>`;
     $("#pool_and_selection").html(items_guide_frame);
     item_display_list ();
+    item_catagory_list();
 
   }
+  function item_catagory_list (){
+    var catagory_frame =  `<div class = "item_cat">
+           <div class="item_list_title">Defense</div>
+           <div class = "item_cat_list">
+              <div class="item_check_box">
+                  <input class ="item_cat_check" type="checkbox" value="Health">
+              </div>
+              <div class= "item_list_name">Health</div>
+           </div>
+           <div class = "item_cat_list">
+             <div class="item_check_box">
+              <input class ="item_cat_check" type="checkbox" value="Armor">
+             </div>
+             <div class= "item_list_name">Armor</div>
+           </div>
+           <div class = "item_cat_list">
+             <div class="item_check_box">
+                <input class ="item_cat_check" type="checkbox" value="SpellBlock">
+             </div>
+             <div class= "item_list_name">Magic Resist</div>
+           </div>
+           <div class = "item_cat_list">
+             <div class="item_check_box">
+                <input class ="item_cat_check" type="checkbox" value="HealthRegen">
+             </div>
+             <div class= "item_list_name">Health Regen</div>
+           </div>
+         </div>
+         <div class = "item_cat">
+           <div class="item_list_title">Attack</div>
+           <div class = "item_cat_list">
+              <div class="item_check_box">
+                  <input class ="item_cat_check" type="checkbox" value="Damage">
+              </div>
+              <div class= "item_list_name">Damage</div>
+           </div>
+           <div class = "item_cat_list">
+              <div class="item_check_box">
+                  <input class ="item_cat_check" type="checkbox" value="CriticalStrike">
+              </div>
+              <div class= "item_list_name">Critical Strike</div>
+           </div>
+           <div class = "item_cat_list">
+              <div class="item_check_box">
+                  <input class ="item_cat_check" type="checkbox" value="AttackSpeed">
+              </div>
+              <div class= "item_list_name">Attack Speed</div>
+           </div>
+           <div class = "item_cat_list">
+              <div class="item_check_box">
+                  <input class ="item_cat_check" type="checkbox" value="LifeSteal">
+              </div>
+              <div class= "item_list_name">Life Steal</div>
+           </div>
+           <div class = "item_cat_list">
+              <div class="item_check_box">
+                  <input class ="item_cat_check" type="checkbox" value="ArmorPenetration">
+              </div>
+              <div class= "item_list_name">Armor Penetration</div>
+           </div>
+         </div>
+         <div class = "item_cat">
+           <div class="item_list_title">Magic</div>
+           <div class = "item_cat_list">
+              <div class="item_check_box">
+                  <input class ="item_cat_check" type="checkbox" value="SpellDamage">
+              </div>
+              <div class= "item_list_name">Ability Power</div>
+           </div>
+           <div class = "item_cat_list">
+              <div class="item_check_box">
+                  <input class ="item_cat_check" type="checkbox" value="CooldownReduction">
+              </div>
+              <div class= "item_list_name">Cooldown Reduction</div>
+           </div>
+           <div class = "item_cat_list">
+              <div class="item_check_box">
+                  <input class ="item_cat_check" type="checkbox" value="Mana">
+              </div>
+              <div class= "item_list_name">Mana</div>
+           </div>
+           <div class = "item_cat_list">
+              <div class="item_check_box">
+                  <input class ="item_cat_check" type="checkbox" value="ManaRegen">
+              </div>
+              <div class= "item_list_name">Mana Regen</div>
+           </div>
+           <div class = "item_cat_list">
+              <div class="item_check_box">
+                  <input class ="item_cat_check" type="checkbox" value="MagicPenetration">
+              </div>
+              <div class= "item_list_name">Magic Penetration</div>
+           </div>
+         </div>
+         <div class = "item_cat">
+           <div class="item_list_title">Movement</div>
+           <div class = "item_cat_list">
+              <div class="item_check_box">
+                  <input class ="item_cat_check" type="checkbox" value="Boots">
+              </div>
+              <div class= "item_list_name">Boots</div>
+           </div>
+           <div class = "item_cat_list">
+              <div class="item_check_box">
+                  <input class ="item_cat_check" type="checkbox" value="NonbootsMovement">
+              </div>
+              <div class= "item_list_name">Others</div>
+           </div>
+         </div>
+         <div class = "item_cat">
+           <div class="item_list_title">Comsumables</div>
+           <div class = "item_cat_list">
+              <div class="item_check_box">
+                  <input class ="item_cat_check" type="checkbox" value="Consumable">
+              </div>
+              <div class= "item_list_name">Consumables</div>
+           </div>
+         </div>`;
+    $(".item_pool_catagory").html(catagory_frame);
+    bind_item_check_box_ui();
+  }
+  function bind_item_check_box_ui(){
+    $('.item_cat_check').on('click',function(){
+      var item_tag = $(this).val();
+      if ($(this).prop("checked")) {
+        local_item_tags.push(item_tag);
+        console.log(local_item_tags);
+      }else {
+        var index = local_item_tags.indexOf(item_tag);
+        local_item_tags.splice(index, 1);
+        console.log(local_item_tags);
+      }
+      item_filter_by_cat();
+    });
+
+  }
+  var item_filter_by_cat = function(){
+    console.log("test, filter");
+    local_item_set = [];
+    var count;
+    var total_tags = local_item_tags.length;
+    for (var i = 0; i < myApp.items.length; i++) {
+      count = 0;
+        // if local_item_tags us ["Health","Armor"];
+        // myApp.items.tags contain both of them will be save into local_item_set
+        // if myApp.items.tags.indexOf(e) > -1, count ++,
+        // if count = local_item_tags.length, which matches the conditions
+      local_item_tags.forEach((e)=>{
+        console.log(e);
+        index =  myApp.items[i].tags.indexOf(e);
+        if (index > -1) {
+          count ++;
+        }
+      });
+      if (count == total_tags) {
+        local_item_set.push(myApp.items[i]);
+      }
+    }
+    $(".item_pool_display").html("");
+    if (local_item_set.length == 0) {
+      $(".item_pool_display").html("No items match by conditions");
+    }
+    for (var i = 0; i < local_item_set.length; i++) {
+      var  item_block = ` <div class = "item_block" id = "item-${local_item_set[i].item_id}">
+                            <div class = "item_icon" data-toggle="popover">
+                              <img src = "../assets/item_icon/`+local_item_set[i].item_id+`.png">
+                            </div>
+                            <div class = "item_name">
+                              `+ local_item_set[i].item_name +`
+                            </div>
+                            <div class = "item_price">
+                              `+ local_item_set[i].total_cost +`
+                            </div>
+                          </div>
+                        `;
+
+      $(".item_pool_display").append(item_block);
+      item_init_popover(local_item_set[i])
+      // console.log(myApp.items[i]._from_);
+    }
+  }
+
+
   function item_display_list (){
     // console.log(myApp.items);
     for (var i = 0; i < myApp.items.length; i++) {
